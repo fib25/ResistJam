c9e1821faaaaaa13b264468e35150d0fc4ef5864
James Provan <jamesprovan25@hotmail.com>
2017-03-11 17:44:35 +0000

    Adding card "csv" parser (actually uses semi-colon separated values). Thanks to Susanna!

--- Assets/ResistJam/Scripts/Editor/CardCollectionEditor.cs
+++ Assets/ResistJam/Scripts/Editor/CardCollectionEditor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System;
 
 [CustomEditor(typeof(CardCollection))]
 public class CardCollectionEditor : Editor
@@ -28,6 +29,56 @@ public class CardCollectionEditor : Editor
 		Debug.Log("Parse");
 		cardCollection.cards.Clear();
 
+		string[] allLines = System.IO.File.ReadAllLines(Application.dataPath + "/ResistJam/StreamingAssets/ResistJamCards.csv");
+		List<List<string>> CardTable = new List<List<string>>();
+		for (int i = 0; i<allLines.Length; i++)
+		{
+			CardTable.Add(new List<string>());
+			string[] result = allLines[i].Split(new string[]{";"},StringSplitOptions.None);
+			foreach (string s in result)
+			{
+				CardTable[i].Add(s);
+			}
+		}
 
+		IdealType currentIdealType = IdealType.Environment;
+
+		for (int i = 1; i<CardTable.Count; i++)
+		{
+			if (CardTable[i][2]!="")
+			{
+				Card newCard = new Card();
+				if (CardTable[i][0] == "ENVIRONMENT")
+				{
+					currentIdealType = IdealType.Environment;
+				}
+				else if (CardTable[i][0] == "CIVIL RIGHTS")
+				{
+					currentIdealType = IdealType.CivilRights;
+				}
+				else if (CardTable[i][0] == "PUBLIC SERVICES")
+				{
+					currentIdealType = IdealType.PublicServices;
+				}
+				else if (CardTable[i][0] == "FOREIGN POLICY")
+				{
+					currentIdealType = IdealType.ForeignPolicy;
+				}
+				else if (CardTable[i][0] == "ECONOMY")
+				{
+					currentIdealType = IdealType.Economy;
+				}
+				else if (CardTable[i][0] == "SCIENCE & CULTURE")
+				{
+					currentIdealType = IdealType.ScienceAndCulture;
+				}
+				newCard.idealType = currentIdealType;
+				newCard.value = float.Parse(CardTable[i][3]);
+				newCard.message = CardTable[i][2];
+
+				cardCollection.cards.Add(newCard);
+			}
+		}
+		Debug.Log(cardCollection.cards.Count);
 	}
 }
